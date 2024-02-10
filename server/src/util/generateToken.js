84f2c244ee21090d59a7806a123d72cd6f53fac6
AliHakim773 <alihakim7732@gmail.com>
2024-02-10 12:20:42 +0200

    Server: added genToken util function

---
+++ server/src/util/generateToken.js
@@ -0,0 +1,12 @@
+import jwt from "jsonwebtoken"
+
+const generateToken = (user) => {
+  const { password: hashedPassword, ...userDetails } = user._doc
+  const token = jwt.sign(userDetails, process.env.JWT_SECRET, {
+    expiresIn: "15d",
+  })
+
+  return token
+}
+
+export default generateToken
