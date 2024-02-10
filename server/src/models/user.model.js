53fd8b2cb2d85d940e4c21f0c8b2e4710e010c67
AliHakim773 <alihakim7732@gmail.com>
2024-02-10 12:20:14 +0200

    Server: Fixed user model

--- server/src/models/user.model.js
+++ server/src/models/user.model.js
@@ -1,7 +1,7 @@
-import { default as mongoose } from "mongoose"
-import { genSalt, hash } from "bcrypt"
+import mongoose from "mongoose"
+import bcrypt from "bcryptjs"
 
-genderEnum = ["male", "female"]
+const genderEnum = ["male", "female"]
 
 const UserSchema = new mongoose.Schema(
   {
@@ -40,8 +40,8 @@ const UserSchema = new mongoose.Schema(
 UserSchema.pre("save", async function (next) {
   if (this.isModified("password") || this.isNew) {
     try {
-      const salt = await genSalt(10)
-      this.password = hash(this.password, salt)
+      const salt = await bcrypt.genSalt(10)
+      this.password = await bcrypt.hash(this.password, salt)
       next()
     } catch (error) {
       console.log("Error in Hasing Password")
