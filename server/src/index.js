6267d4b1e39dcba5d3d9281261a4021c1ee155cf
AliHakim773 <alihakim7732@gmail.com>
2024-02-10 12:19:58 +0200

    Server: Added middlewares

--- server/src/index.js
+++ server/src/index.js
@@ -1,13 +1,18 @@
-import dotenv from "dotenv"
-import express from "express"
+import { config } from "dotenv"
+import express, { json } from "express"
 import connectToMongoDB from "./configs/db.config.js"
 import siteRoutes from "./routes/index.routes.js"
+import errorMiddleware from "./middlewares/error.middleware.js"
+import cookieParser from "cookie-parser"
 
-dotenv.config()
+config()
 
 const app = express()
+app.use(json())
+app.use(cookieParser())
 
 siteRoutes(app)
+app.use(errorMiddleware)
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
