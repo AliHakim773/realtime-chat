887de251646d668a01731023cb2f3cfd76a9d8f7
AliHakim773 <alihakim7732@gmail.com>
2024-02-10 12:42:23 +0200

    Server: Added login and logout functions

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -2,6 +2,7 @@ import errorHandler from "../util/errorHandler.js"
 import generateToken from "../util/generateToken.js"
 import setCookie from "../util/setCookie.js"
 import User from "../models/user.model.js"
+import bcrypt from "bcryptjs"
 
 export const register = async (req, res, next) => {
   const { fullName, username, password, confirmPassword, gender } = req.body
@@ -24,14 +25,11 @@ export const register = async (req, res, next) => {
     })
     await newUser.save()
 
-    const token = generateToken(newUser)
+    const [token, userDetails] = generateToken(newUser)
     setCookie(token, res)
 
-    res.status(201).json({
-      _id: newUser._id,
-      fullName: newUser.fullName,
-      username: newUser.username,
-      profilePic: newUser.profilePic,
+    res.status(201).send({
+      ...userDetails,
     })
   } catch (e) {
     next(e)
@@ -39,9 +37,32 @@ export const register = async (req, res, next) => {
 }
 
 export const login = async (req, res, next) => {
-  res.send("login")
+  const { username, password } = req.body
+  try {
+    const validUser = await User.findOne({ username })
+    if (!validUser) return next(errorHandler(401, "Wrong Credentials!"))
+
+    const isValidPassword = bcrypt.compareSync(password, validUser.password)
+    if (!isValidPassword) return next(errorHandler(401, "Wrong Credentials!"))
+
+    const [token, userDetails] = generateToken(validUser)
+    setCookie(token, res)
+
+    res.status(200).send({
+      ...userDetails,
+    })
+  } catch (error) {
+    next(error)
+  }
 }
 
 export const logout = async (req, res, next) => {
-  res.send("logout")
+  try {
+    res
+      .clearCookie("access_token")
+      .status(200)
+      .send({ message: "User has been logged out" })
+  } catch (error) {
+    next(error)
+  }
 }
